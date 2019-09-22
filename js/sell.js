ea1843b0226cb7cbb45901762d3c37010510b7f3
Austin Wise <austin.k.wise@gmail.com>
2019-09-21 23:12:15 -0400

    Add files via upload

---
+++ js/sell.js
@@ -0,0 +1,26 @@
+
+
+//CLICKABLE ELEMENTS
+const submitBtn = document.getElementById();//get the GET YOUR CASH OFFER button
+
+//DATA INPUT ELEMENTS
+const nameField = document.getElementById();//get the name field
+const countyField = document.getElementById();//get the county field
+const stateField = document.getElementById();//get the state field
+const phoneField = document.getElementById();//get the phone field
+const emailField = document.getElementById();//get the email field
+
+//DATA OUTPUT ELEMENTS
+//no output variables
+
+//FUNCTIONS
+
+submitBtn.addEventListener("click", function submitBtnClick() {
+    //get text from name, county, state and phone fields
+
+    //add text to database
+
+    //push text to next page
+
+    //take user to next page
+})
